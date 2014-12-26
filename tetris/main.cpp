baa007d6be06d2ac24b47f46e01cdc72e363e5ec
benlamonica <ben.lamonica@gmail.com>
2014-12-26 08:12:25 -0600

    Initial Commit

---
+++ tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tetris
+//
+//  Created by La Monica Family on 12/26/14.
+//  Copyright (c) 2014 Benjamin Alan La Monica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
